8aba2f8ea0ac0fdd7f4c7f92850eeec197b69ea0
Michael Melvin <mjm.neuro@gmail.com>
2024-03-01 08:19:39 -0500

    Escrow: make passing

--- escrow/tests/escrow.ts
+++ escrow/tests/escrow.ts
@@ -2,39 +2,67 @@ import * as anchor from "@coral-xyz/anchor";
 
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
-  Account,
+  MINT_SIZE,
   TOKEN_PROGRAM_ID,
-  createMint,
-  getAssociatedTokenAddress,
-  getOrCreateAssociatedTokenAccount,
+  createAssociatedTokenAccountIdempotentInstruction,
+  createInitializeMint2Instruction,
+  createMintToInstruction,
+  getAssociatedTokenAddressSync,
+  getMinimumBalanceForRentExemptMint,
 } from "@solana/spl-token";
-import { Keypair, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
+import {
+  Keypair,
+  LAMPORTS_PER_SOL,
+  PublicKey,
+  Transaction,
+} from "@solana/web3.js";
 
 import { Escrow } from "../target/types/escrow";
 import { Program } from "@coral-xyz/anchor";
 import { before } from "mocha";
+import { min } from "bn.js";
 import { randomBytes } from "crypto";
 
 describe("escrow", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
-
+  const provider = anchor.getProvider();
   const program = anchor.workspace.Escrow as Program<Escrow>;
-  const maker = anchor.Wallet.local();
+  const maker = Keypair.generate();
   const taker = Keypair.generate();
   const connection = program.provider.connection;
   const amountX = new anchor.BN(100 * LAMPORTS_PER_SOL);
   const amountY = new anchor.BN(100 * LAMPORTS_PER_SOL);
 
   let seed = new anchor.BN(randomBytes(8));
-  let escrow: PublicKey;
-  let mintX: PublicKey;
-  let mintY: PublicKey;
-  let makerAtaX: Account;
-  let makerAtaY: Account;
-  let takerAtaX: Account;
-  let takerAtaY: Account;
-  let escrowAtaX: PublicKey;
+  let escrow = PublicKey.findProgramAddressSync(
+    [
+      Buffer.from("escrow"),
+      maker.publicKey.toBuffer(),
+      seed.toArrayLike(Buffer, "le", 8),
+    ],
+    program.programId
+  )[0];
+
+  let mintX = Keypair.generate();
+  let mintY = Keypair.generate();
+  const makerAtaX = getAssociatedTokenAddressSync(
+    mintX.publicKey,
+    maker.publicKey
+  );
+  const makerAtaY = getAssociatedTokenAddressSync(
+    mintY.publicKey,
+    maker.publicKey
+  );
+  const takerAtaX = getAssociatedTokenAddressSync(
+    mintX.publicKey,
+    taker.publicKey
+  );
+  const takerAtaY = getAssociatedTokenAddressSync(
+    mintY.publicKey,
+    taker.publicKey
+  );
+  let escrowAtaX = getAssociatedTokenAddressSync(mintX.publicKey, escrow, true);
 
   const confirm = async (signature: string): Promise<string> => {
     const block = await connection.getLatestBlockhash();
@@ -52,71 +80,97 @@ describe("escrow", () => {
     return signature;
   };
 
-  before(async () => {
-    await connection
-      .requestAirdrop(maker.publicKey, LAMPORTS_PER_SOL * 10)
-      .then(confirm)
-      .then(log);
-    await connection
-      .requestAirdrop(taker.publicKey, LAMPORTS_PER_SOL * 10)
-      .then(confirm)
-      .then(log);
+  it("Airdrop", async () => {
+    await Promise.all([
+      await connection
+        .requestAirdrop(maker.publicKey, LAMPORTS_PER_SOL * 10)
+        .then(confirm),
+      await connection
+        .requestAirdrop(taker.publicKey, LAMPORTS_PER_SOL * 10)
+        .then(confirm),
+    ]);
+  });
 
-    mintX = await createMint(connection, maker.payer, maker.publicKey, null, 9);
-    mintY = await createMint(connection, maker.payer, maker.publicKey, null, 9);
-    makerAtaX = await getOrCreateAssociatedTokenAccount(
-      connection,
-      maker.payer,
-      mintX,
-      maker.publicKey
-    );
-    makerAtaY = await getOrCreateAssociatedTokenAccount(
-      connection,
-      maker.payer,
-      mintY,
-      maker.publicKey
-    );
-    takerAtaX = await getOrCreateAssociatedTokenAccount(
-      connection,
-      taker,
-      mintX,
-      taker.publicKey
-    );
-    takerAtaY = await getOrCreateAssociatedTokenAccount(
-      connection,
-      taker,
-      mintX,
-      taker.publicKey
-    );
-    escrow = PublicKey.findProgramAddressSync(
-      [
-        Buffer.from("escrow"),
-        maker.publicKey.toBuffer(),
-        seed.toArrayLike(Buffer, "le", 8),
-      ],
-      program.programId
-    )[0];
-    escrowAtaX = await getAssociatedTokenAddress(mintX, escrow, true);
+  it("Setup mint", async () => {
+    const lamports = await getMinimumBalanceForRentExemptMint(connection);
+    let tx = new Transaction();
+    tx.instructions = [
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: provider.publicKey,
+        newAccountPubkey: mintX.publicKey,
+        lamports,
+        space: MINT_SIZE,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: provider.publicKey,
+        newAccountPubkey: mintY.publicKey,
+        lamports,
+        space: MINT_SIZE,
+        programId: TOKEN_PROGRAM_ID,
+      }),
+      createInitializeMint2Instruction(
+        mintX.publicKey,
+        6,
+        maker.publicKey,
+        null
+      ),
+      createInitializeMint2Instruction(
+        mintY.publicKey,
+        6,
+        taker.publicKey,
+        null
+      ),
+      createAssociatedTokenAccountIdempotentInstruction(
+        provider.publicKey,
+        makerAtaX,
+        maker.publicKey,
+        mintX.publicKey
+      ),
+      createAssociatedTokenAccountIdempotentInstruction(
+        provider.publicKey,
+        takerAtaY,
+        taker.publicKey,
+        mintY.publicKey
+      ),
+      createMintToInstruction(
+        mintX.publicKey,
+        makerAtaX,
+        maker.publicKey,
+        1000e9
+      ),
+      createMintToInstruction(
+        mintY.publicKey,
+        takerAtaY,
+        taker.publicKey,
+        1000e9
+      ),
+    ];
+
+    try {
+      await provider.sendAndConfirm(tx, [mintX, mintY, maker, taker]).then(log);
+    } catch (error) {
+      console.log(error);
+    }
   });
 
   it("make", async () => {
     await program.methods
-      .make(amountX, amountX, seed)
+      .make(seed, amountX, amountX)
       .accounts({
         maker: maker.publicKey,
         escrow,
-        mintX,
-        mintY,
+        mintX: mintX.publicKey,
+        mintY: mintY.publicKey,
         escrowAtaX,
-        makerAtaX: makerAtaX.address,
-        makerAtaY: makerAtaY.address,
+        makerAtaX: makerAtaX,
+        makerAtaY: makerAtaY,
         associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([maker.payer])
+      .signers([maker])
       .rpc()
-      .then(confirm)
       .then(log);
   });
 
@@ -127,12 +181,12 @@ describe("escrow", () => {
         taker: taker.publicKey,
         maker: maker.publicKey,
         escrow,
-        mintX,
-        mintY,
+        mintX: mintX.publicKey,
+        mintY: mintY.publicKey,
         escrowAtaX,
-        takerAtaX: takerAtaX.address,
-        takerAtaY: takerAtaY.address,
-        makerAtaY: makerAtaY.address,
+        takerAtaX: takerAtaX,
+        takerAtaY: takerAtaY,
+        makerAtaY: makerAtaY,
         associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
@@ -153,23 +207,23 @@ describe("escrow", () => {
       ],
       program.programId
     )[0];
-    escrowAtaX = await getAssociatedTokenAddress(mintX, escrow, true);
+    escrowAtaX = getAssociatedTokenAddressSync(mintX.publicKey, escrow, true);
 
     await program.methods
       .make(amountX, amountX, seed)
       .accounts({
         maker: maker.publicKey,
         escrow,
-        mintX,
-        mintY,
+        mintX: mintX.publicKey,
+        mintY: mintY.publicKey,
         escrowAtaX,
-        makerAtaX: makerAtaX.address,
-        makerAtaY: makerAtaY.address,
+        makerAtaX: makerAtaX,
+        makerAtaY: makerAtaY,
         associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([maker.payer])
+      .signers([maker])
       .rpc()
       .then(confirm)
       .then(log);
@@ -179,15 +233,15 @@ describe("escrow", () => {
       .accounts({
         maker: maker.publicKey,
         escrow,
-        mintX,
-        mintY,
+        mintX: mintX.publicKey,
+        mintY: mintY.publicKey,
         escrowAtaX,
-        makerAtaX: makerAtaX.address,
+        makerAtaX: makerAtaX,
         associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
         tokenProgram: TOKEN_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
       })
-      .signers([maker.payer])
+      .signers([maker])
       .rpc()
       .then(confirm)
       .then(log);
