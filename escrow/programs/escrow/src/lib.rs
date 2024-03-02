0fa2e6cbbd4d39574bcb9cb70897344ae1da4482
Michael Melvin <mjm.neuro@gmail.com>
2024-03-01 20:51:59 -0500

    Escrow: minor refactors

--- escrow/programs/escrow/src/lib.rs
+++ escrow/programs/escrow/src/lib.rs
@@ -12,8 +12,7 @@ pub mod escrow {
     use super::*;
 
     pub fn make(ctx: Context<Make>, seed: u64, amount_x: u64, amount_y: u64) -> Result<()> {
-        ctx.accounts
-            .make(seed, amount_x, amount_y, &MakeBumps::from(ctx.bumps))
+        ctx.accounts.make(seed, amount_x, amount_y, &ctx.bumps)
     }
 
     pub fn refund(ctx: Context<Refund>) -> Result<()> {
